f4885d4ccf742bc528806dcfc742599f1089119f
unknown <hanssbtn@gmail.com>
2024-04-01 16:28:26 +0700

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config({path: 'sample.env'});
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+var multer = require('multer');
+const uploads = multer({'dest': 'uploads/'});
 
 var app = express();
 
@@ -11,7 +13,14 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.use(uploads.single('upfile'));
+app.post('/api/fileanalyse', function (req, res) {
+    res.json({
+        name: req.file.originalname,
+        type: req.file.mimetype,
+        size: req.file.size
+    });
+});
 
 
 const port = process.env.PORT || 3000;
